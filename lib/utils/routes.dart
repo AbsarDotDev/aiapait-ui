5329d1e74533f492b89e212374ccc77ecae18163
Absar Ali <absarali701@gmail.com>
2023-09-14 14:41:09 +0500

    Routes setup with splash screen and login

---
+++ lib/utils/routes.dart
@@ -0,0 +1,27 @@
+import 'package:aiapait/screens/login_screen.dart';
+import 'package:aiapait/screens/splash_screen.dart';
+import 'package:aiapait/utils/route_names.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteNames.splashScreen:
+        return MaterialPageRoute(
+          builder: (BuildContext context) => const SplashScreen(),
+        );
+      case RouteNames.loginScreen:
+        return MaterialPageRoute(
+          builder: (BuildContext context) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return const Scaffold(
+            body: Center(
+              child: Text("No Route Found"),
+            ),
+          );
+        });
+    }
+  }
+}
